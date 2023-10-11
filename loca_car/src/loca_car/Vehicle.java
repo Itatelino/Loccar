41b8dcff92c6b9c3da3690011e0fa7248f28bc02
Itatelino de Oliveira Leite Neto <itatelino.oliveira@gmail.com>
2023-10-11 14:04:54 -0300

    Primeiro Commit

---
+++ loca_car/src/loca_car/Vehicle.java
@@ -0,0 +1,24 @@
+package loca_car;
+
+public class Vehicle {
+
+		private String model;
+		
+		public Vehicle() {
+			
+		}
+		
+		public Vehicle(String model) {
+			this.model = model;
+		}
+		
+		public String getModel() {
+			return model;
+		}
+		
+		public void setModel(String mode) {
+			this.model = model;
+		}
+		
+}
+	
